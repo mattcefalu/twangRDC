e0671197adaf8c398b5e4d71e6546af9dcd1d783
Cefalu <mcefalu@rand.org>
2021-03-22 07:53:23 -0600

    Initial commit

---
+++ R/data.r
@@ -0,0 +1,23 @@
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{c00_age12}{price, in US dollars}
+#'   \item{c00_nphu}{weight of the diamond, in carats}
+#'   \item{c00_race}{weight of the diamond, in carats}
+#'   \item{c00_sex}{weight of the diamond, in carats}
+#'   \item{concdis}{something}
+#'   \item{hhid}{weight of the diamond, in carats}
+#'   \item{id}{weight of the diamond, in carats}
+#'   \item{imm_conc}{weight of the diamond, in carats}
+#'   \item{metarea}{weight of the diamond, in carats}
+#'   \item{nola_rec}{weight of the diamond, in carats}
+#'   \item{res_stab}{weight of the diamond, in carats}
+#'   \item{sim_pik}{weight of the diamond, in carats}
+#'   \item{tract_id_str}{weight of the diamond, in carats}
+#' }
+#' @usage data(nola_south)
+"nola_south"
